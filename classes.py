ab3ec5737524d874363cb48a1da5fa2d8e5419f0
shabeeb-ameen <shabeeb.ameen@gmail.com>
2024-03-02 16:33:38 -0500

    FIRE minimization working for polygons

--- classes.py
+++ classes.py
@@ -5,6 +5,8 @@ class Vertex:
         self.position_ = np.array(position)
         self.force_ = np.zeros(2)
         self.velocity_ = np.zeros(2)
+        self.is_fixed_ = False
+        self.is_linked_ = False
         return
 
 class Polygon:
